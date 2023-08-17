35ac6d2c26c6a81031263eb3b6d250d6676b947c
BHAVIKA <Bhavikapathak111@gmail.com>
2023-08-17 14:59:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+conda create -p venv python=3.8
+
+conda activate venv/
+
+FROM python:3.8-slim-buster
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+CMD [ "python3","app.py" ]
---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+MONGO_DATABASE_NAME = "mlproject"
+MONGO_COLLECTION_NAME = "waferfaultdetection"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://BhavikaPathak:bhavika@cluster0.xevimge.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
